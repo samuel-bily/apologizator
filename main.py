71a029bcaef5b3cd8a1273936450a7e529901869
thedominica <domi.cupkova@gmail.com>
2021-02-12 02:17:30 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -5,23 +5,19 @@ from textgenrnn import textgenrnn
 
 app = Flask(__name__)
 
-
-@app.route('/favicon.ico')
-def favicon():
+@app.route('/favicon.ico') 
+def favicon(): 
     return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
 @app.route('/generate', methods=['POST'])
 def generate():
-    textgen = textgenrnn("apologies_noapostro_maxlength_weights.hdf5")
+    textgen = textgenrnn("textgenrnn_weights_4epochs.hdf5")
     lyrics = textgen.generate(n=20, return_as_list=True)
     return "\n".join(lyrics)
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, threaded=False)
