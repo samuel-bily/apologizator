92aaac15d9dbdabef798855c7e8bf611fc6f1c90
thedominica <domi.cupkova@gmail.com>
2021-02-13 04:48:26 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import boto3
 import os
 from textgenrnn import textgenrnn
 
-app = Flask(__name__, static_folder='hello-react', static_url_path='')
+app = Flask(__name__, static_folder='hello-react/static', static_url_path='')
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
