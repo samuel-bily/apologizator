dccd0420e2ab4577e55c13281506c697a5b2e8b4
thedominica <domi.cupkova@gmail.com>
2021-02-20 20:29:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def index():
    
 @app.route('/favicon.ico') 
 def favicon(): 
-    return send_from_directory(os.path.join(app.root_path, 'root'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 @app.route('/generate', methods=['GET'])
 def generate():
